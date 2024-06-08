b6a075b3e61ea4eaffc787d104043909111ef030
Yoga Novaindra <yoga.november2000@gmail.com>
2024-06-08 16:31:41 +0700

    action deploy

--- src/routes/imageUploadRoutes.ts
+++ src/routes/imageUploadRoutes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { accessValidation } from "../middleware/authMiddleware";
 import { handicraftValidation } from "../middleware/handicraftMiddleware";
 import { upload } from "../utils/multer";
-import { updateImageHandicraft, updateImageDetailHandicraft } from "../controllers/imageUploadController";
+import { updateImageHandicraft, updateImageDetailHandicraft, updateImageUser } from "../controllers/imageUploadController";
 
 const router = Router();
 
 router.put("/handicraft/:id", accessValidation, handicraftValidation, upload.single("image"), updateImageHandicraft);
 router.put("/detailhandicraft/:id", accessValidation, handicraftValidation, upload.single("image"), updateImageDetailHandicraft);
+router.put("/user/:id", accessValidation, upload.single("image"), updateImageUser);
 
 export default router;
