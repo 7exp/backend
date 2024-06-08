848a0309a99aee91f0b97d24298bbdcd001f2c97
Yoga Novaindra <yoga.november2000@gmail.com>
2024-06-08 17:00:00 +0700

    add deployment bash script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Function to print success message
 function success_message() {
   echo -e "\033[0;32m✔ $1\033[0m"
@@ -8,20 +7,16 @@ function success_message() {
 # Function to print error message
 function error_message() {
   echo -e "\033[0;31m✘ Error: $1\033[0m"
-  exit 1
 }
 
-# Navigate to the backend directory
-cd ~/backend || error_message "Failed to change directory"
-
 # Update the Git repository
-git pull && success_message "Git repository updated" || error_message "Failed to update Git repository"
+git pull && success_message "Git repository updated" || { error_message "Failed to update Git repository"; exit 1; }
 
 # Pull the latest Docker images
-sudo docker-compose pull && success_message "Docker images pulled" || error_message "Failed to pull Docker images"
+sudo docker-compose pull && success_message "Docker images pulled" || { error_message "Failed to pull Docker images"; exit 1; }
 
 # Start your Docker containers
-sudo docker-compose up -d && success_message "Docker containers started" || error_message "Failed to start Docker containers (check logs with 'docker-compose logs')"
+sudo docker-compose up -d && success_message "Docker containers started" || { error_message "Failed to start Docker containers (check logs with 'docker-compose logs')"; exit 1; }
 
 # Remove unused Docker images
-sudo docker image prune -af && success_message "Unused Docker images removed" || error_message "Failed to prune Docker images"
+sudo docker image prune -af && success_message "Unused Docker images removed" || { error_message "Failed to prune Docker images"; exit 1; }
